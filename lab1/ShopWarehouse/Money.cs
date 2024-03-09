33c8e41eeaec32b43b23c28ac5c96bc8b3b20815
Anatolii Kupriienko <takblet7331228322@gmail.com>
2024-03-10 00:43:26 +0200

    Created Money class

---
+++ lab1/ShopWarehouse/Money.cs
@@ -0,0 +1,50 @@
+﻿namespace ShopWarehouse;
+
+public class Money
+{
+    public int Whole
+    {
+        get
+        {
+            return this.Whole;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                this.Whole = value;
+            }
+        }
+    }
+    public int Fraction
+    {
+        get
+        {
+            return this.Fraction;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                this.Fraction = value;
+            }
+        }
+    }
+
+    public Money(int whole, int fraction)
+    {
+        this.Whole = whole;
+        this.Fraction = fraction;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(this.Whole + "." + this.Fraction);
+    }
+
+    public void SetNewSum(int whole, int fraction)
+    {
+        this.Whole = whole;
+        this.Fraction = fraction;
+    }
+}
